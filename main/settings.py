e9471c1e983ea4016dd0cf86050355c1d58db491
Jonnathan23 <jsaquicela26@gmail.com>
2025-01-06 09:48:31 -0500

    pocos cambios

--- main/settings.py
+++ main/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'main.wsgi.app'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
